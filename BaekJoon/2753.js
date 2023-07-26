e3d7b67c1b1f6666d60155cc5ce2421b1d5dbfa8
binwoonki <qlsdnsrl54@gmail.com>
2023-07-26 17:46:02 +0900

    [백준] 2753 윤년 구하기

---
+++ BaekJoon/2753.js
@@ -0,0 +1,10 @@
+const fs = require('fs');
+const input = fs.readFileSync('/dev/stdin').toString().split(' ');
+
+const leapYear = Number(input);
+
+if((leapYear % 4 === 0 && leapYear % 100 !== 0) || leapYear % 400 === 0){
+    console.log("1")
+}else{
+    console.log("0")
+}
